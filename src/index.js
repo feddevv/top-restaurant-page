d301e2fcf54f55b98961304e7270bf422a11d96d
Fedya Styopochkin <fst20071221@gmail.com>
2026-03-05 13:22:32 +0000

    Add event listeners for navigation buttons

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ import homePage from './components/homePage.js'
 import menuPage from './components/menuPage.js'
 import aboutPage from './components/aboutPage.js'
 
-aboutPage()
+const homeButton = document.getElementById('home')
+const menuButton = document.getElementById('menu')
+const aboutButton = document.getElementById('about')
+
+homeButton.addEventListener('click', homePage)
+menuButton.addEventListener('click', menuPage)
+aboutButton.addEventListener('click', aboutPage)
+
+homePage()
